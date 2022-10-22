4b69c70f4419be3e41772fca907a815e3b45936c
Nurik <you@example.com>
2022-10-23 00:26:25 +0300

    second digit of number

---
+++ Second_Digit/Program.cs
@@ -0,0 +1,17 @@
+﻿
+/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int SecondDigitOfNumber(int a )
+{
+    int b = a /10  % 10;
+    
+    return b;
+}
+Console.Write("Type the  triple number: ");
+int a  = int.Parse(Console.ReadLine()!);
+ Console.WriteLine(SecondDigitOfNumber(a));

f6b25f24df8b4191dfb5ffd2ea0ae6d85afc973c
Nurik <you@example.com>
2022-10-23 01:51:41 +0300

    diving to 7 and 23

---
+++ Multiplicity_To_7_And_23/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Дополнительная Задача 9
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+14 -> нет
+46 -> нет
+161 -> да
+*/
+
+Console.Write("Type any number: ");
+int a  = int.Parse(Console.ReadLine()!);
+
+if (a % (7 * 23) == 0)
+{
+    Console.WriteLine("yep!");
+}
+else 
+{
+    Console.WriteLine("no!");
+}

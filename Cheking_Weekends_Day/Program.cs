b52df79ce3118bcdbdd37481ede3520591225bf3
Nurik <you@example.com>
2022-10-23 01:12:32 +0300

    cheking weekends day done

---
+++ Cheking_Weekends_Day/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Type  number: ");
+int a  = int.Parse(Console.ReadLine()!);
+
+if ( a == 6 || a == 7)
+{
+    Console.WriteLine("yep!");
+}
+
+else if (a == new Random().Next(1, 6) )
+{
+    Console.WriteLine("no!");
+}
+else
+{
+  Console.WriteLine("Please, type the number in (1,7) range! ");
+}

107ddcf17014ac733e3f8334eb0a6d3c5972b0a2
Nurik <you@example.com>
2022-10-23 00:27:33 +0300

    third digit of number

---
+++ Third_Digit/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int ThirdDigitOFnumber(int a)
+{
+    while(a >= 1000){
+    a = a /10;
+    }
+     
+    return a % 10;
+}
+    
+Console.Write("Type any number: ");
+int a  = int.Parse(Console.ReadLine()!);
+
+if ( a >= 1000)
+{
+    Console.WriteLine(ThirdDigitOFnumber(a));
+}
+ else if  (a < 100)
+{
+    Console.WriteLine("there isn't third digit");
+}
+else 
+{
+ Console.WriteLine(a % 10);
+}
